21613ee30c19775e996c1abeec0ed7463a56ac8a
Parth Sharma Poudel <parthpoudel11@gmail.com>
2026-01-26 21:08:22 +0000

    reset main file

--- kv-store/cmd/kvserve/main.go
+++ kv-store/cmd/kvserve/main.go
@@ -1,17 +1,1 @@
 package main
-
-import (
-	"net/http"
-
-	"github.com/isparth/Distributed-Systems/kv-store/internal/httpapi"
-	"github.com/isparth/Distributed-Systems/kv-store/internal/kv"
-)
-
-func main() {
-	kv := kv.NewKVStore(kv.SnapshotOnly)
-
-	handler := httpapi.NewRouter(kv)
-
-	_ = http.ListenAndServe(":8080", handler)
-
-}
